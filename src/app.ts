38d53acd1b04d17bf86a5c71d7b440c72bc01151
hasibulofcs <hasibulofcs@gmail.com>
2024-12-20 20:29:31 +0600

    updates on linting

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,10 @@ const port = config.port || 3000;
 app.use(express.json());
 app.use(cors());
 
+console.log(app);
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Hello World I am from app!");
 });
 
 export default app;
