46796f549318e8c7944a98e63f69182908284812
hasibulofcs <hasibulofcs@gmail.com>
2024-12-25 19:15:22 +0600

    updated README

---
+++ README.md
@@ -0,0 +1,8 @@
+# `E2 E-commerce Mini Backend`
+
+- ### https://e2ecommercemini.vercel.app/
+- ### api starter
+
+```bash
+https://e2ecommercemini.vercel.app/api/v1
+```
--- dist/app.js
+++ dist/app.js
@@ -8,10 +8,18 @@ const cors_1 = __importDefault(require("cors"));
 const routes_1 = require("./app/routes");
 const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalErrorHandler"));
 const notFound_1 = __importDefault(require("./app/middlewares/notFound"));
+const http_status_codes_1 = require("http-status-codes");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use("/api/v1", routes_1.AppRoutes);
+app.get("/", (_req, res) => {
+    res.status(http_status_codes_1.StatusCodes.OK).json({
+        success: true,
+        status: http_status_codes_1.StatusCodes.OK,
+        message: "Welcome to E-commerce app API",
+    });
+});
 app.use(globalErrorHandler_1.default);
 app.use(notFound_1.default);
 exports.default = app;
