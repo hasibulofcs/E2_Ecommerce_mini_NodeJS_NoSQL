0eeccad529d7d4e808a9aa37cfe39f1cc00f375a
hasibulofcs <hasibulofcs@gmail.com>
2024-12-25 20:25:59 +0600

    removed dist folder from repo

--- dist/app/modules/orders/orders.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OrderRoutes = void 0;
-const express_1 = require("express");
-const orders_controller_1 = require("./orders.controller");
-const router = (0, express_1.Router)();
-router.post("/", orders_controller_1.OrderController.createNewOrder);
-router.get("/", orders_controller_1.OrderController.getAllOrders);
-exports.OrderRoutes = router;
